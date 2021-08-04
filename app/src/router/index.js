6e32652042ee850664f3fa98a155a9be27928fe6
ZiYooh <natsucomi42@gmail.com>
2021-08-04 16:28:47 +0900

    공지사항, 고객센터 페이지 라우터 등록

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -5,6 +5,8 @@ import CorpInfo from '@/components/CorpInfo';
 import VuetifyTest from '@/components/VuetifyTest';
 import ConnTest from '@/components/ConnTest';
 import Candidate from '@/components/Candidate';
+import Notice from '@/components/Notice';
+import Help from '@/components/Help';
 
 Vue.use(Router);
 
@@ -34,6 +36,16 @@ export default new Router({
       path: '/candidate',
       name: 'Candidate',
       component: Candidate,
+    },
+    {
+      path: '/notice',
+      name: 'Notice',
+      component: Notice,
+    },
+    {
+      path: '/help',
+      name: 'Help',
+      component: Help,
     },
   ],
 });
