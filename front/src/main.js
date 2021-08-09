aadca7939d87a781522223c63276abb6f429fd29
ZiYooh <natsucomi42@gmail.com>
2021-08-09 17:26:19 +0900

    Web3 전역 사용을 위한 Vue 믹스인 설정

--- front/src/main.js
+++ front/src/main.js
@@ -1,11 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Web3 from 'web3';
 import vuetify from '@/plugins/vuetify';
 import App from './App';
 import router from './router';
+import Config from './config';
 
 Vue.config.productionTip = false;
+
+Vue.mixin({
+  created() {
+    const web3js = window.web3;
+    if (typeof web3js !== 'undefined') {
+      this.$web3 = new Web3(web3js.currentProvider);
+    }
+
+    this.$getDefaultAccount = () => new Promise((resolve, reject) => {
+      this.$web3.eth.getAccounts((err, data) => {
+        if (!err) resolve(data[0]);
+        reject(err);
+      });
+    });
+    this.$config = Config;
+  },
+});
 
 /* eslint-disable no-new */
 new Vue({
