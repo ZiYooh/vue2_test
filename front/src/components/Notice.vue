a2f9e2da1309309572d61a8e508cc74c643c0a2f
ZiYooh <natsucomi42@gmail.com>
2021-08-15 02:23:01 +0900

    공지사항 페이지 틀 제작

--- front/src/components/Notice.vue
+++ front/src/components/Notice.vue
@@ -1,6 +1,40 @@
 <template>
   <v-content class="mx-15 mt-15">
     <h1>공지사항 페이지 입니다</h1>
+    <v-simple-table class="mx-15" dense>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              번호
+            </th>
+            <th class="text-left">
+              제목
+            </th>
+            <th class="text-left">
+              작성자
+            </th>
+            <th class="text-left">
+              작성일
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>02</td>
+            <td>서버 점검 입니다</td>
+            <td>관리자</td>
+            <td>2021/01/02</td>
+          </tr>
+          <tr>
+            <td>01</td>
+            <td>공지사항 입니다</td>
+            <td>관리자</td>
+            <td>2021/01/01</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </v-content>
 </template>
 
