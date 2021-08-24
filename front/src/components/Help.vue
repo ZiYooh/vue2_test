b137d35d50d758ba47200a1bd192fe0a5af62193
ZiYooh <natsucomi42@gmail.com>
2021-08-24 17:41:25 +0900

    고객센터 틀 제작중 2

--- front/src/components/Help.vue
+++ front/src/components/Help.vue
@@ -1,7 +1,7 @@
 <template>
   <v-content class="mx-15 mt-15">
-    <v-row justify="center">
-      <h2>자주 묻는 질문(FAQ)</h2>
+    <v-row justify="center" class="mb-5">
+      <h2 class="mb-3">자주 묻는 질문(FAQ)</h2>
       <v-expansion-panels inset>
         <v-expansion-panel>
           <v-expansion-panel-header>메타마스크 연동 방법</v-expansion-panel-header>
@@ -35,6 +35,42 @@
         </v-expansion-panel>
       </v-expansion-panels>
     </v-row>
+
+    <h2 class="text-center mb-3">묻고 답하기(Q&A)</h2>
+    <v-simple-table class="mx-15" dense>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              번호
+            </th>
+            <th class="text-left">
+              제목
+            </th>
+            <th class="text-left">
+              작성자
+            </th>
+            <th class="text-left">
+              작성일
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>02</td>
+            <td>투표 개설 질문 입니다</td>
+            <td>기업 회원1</td>
+            <td>2021/01/02</td>
+          </tr>
+          <tr>
+            <td>01</td>
+            <td>질문 입니다</td>
+            <td>회원1</td>
+            <td>2021/01/01</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </v-content>
 </template>
 
