3df0dc49f88f1fc76ff156fec56b599d68e57d03
ZiYooh <natsucomi42@gmail.com>
2021-09-07 23:10:11 +0900

    주석 추가

--- front/src/config.js
+++ front/src/config.js
@@ -1,6 +1,7 @@
 const Hello = require('../../back/build/contracts/Hello.json');
 
 export default {
+  // 5777 = 가나슈 네트워크 번호로, 다른 네트워크에 배포 시 수정해야함
   HELLO_CA: Hello.networks[5777].address,
   HELLO_ABI: Hello.abi,
   GAS_AMOUNT: 500000,
