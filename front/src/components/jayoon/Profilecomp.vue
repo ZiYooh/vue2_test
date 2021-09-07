c23c274f1285e83243e1c19275c92bc826f6e06d
ZiYooh <natsucomi42@gmail.com>
2021-09-07 16:57:23 +0900

    백엔드 ~ 프론트엔드간 연동 완료

---
+++ front/src/components/jayoon/Profilecomp.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="container">
+		<div class="jumbotron mt-5">
+			<div class="col-sm-8 mx-auto">
+				<h1 class="text-center">My Profile</h1>
+			</div>
+
+			<table class="table col-md-6 mt-5 mx-auto">
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col">기업명</th>
+						<td scope="col">{{ compName }}</td>
+					</tr>
+				</thead>
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col">기업번호</th>
+						<td scope="col">{{ compNum }}</td>
+					</tr>
+				</thead>
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col">이메일</th>
+						<td scope="col">{{ email }}</td>
+					</tr>
+                </thead>
+			</table>
+		</div>
+	</div>
+</template>
+
+<script>
+import jwtDecode from "jwt-decode";
+export default {
+	data() {
+		const token = localStorage.usertoken;
+		const decoded = jwtDecode(token);
+		return {
+			compName: decoded.compName,
+			compNum: decoded.compNum,
+			email: decoded.email,
+		};
+	},
+};
+</script>
+
+<style scoped>
+td {
+	background-color: white;
+}
+</style>
---
+++ front/src/components/jayoon/UserListcomp.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="container">
+		<div class="jumbotron mt-5">
+			<div class="col-sm-8 mx-auto">
+				<h1 class="text-center">User List</h1>
+			</div>
+			<table class="table mt-5 mx-auto">
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">기업명</th>
+						<th scope="col">기업번호</th>
+						<th scope="col">이메일</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="(user, index) in users" :key="user.id">
+						<td>{{ index + 1 }}</td>
+						<td>{{ user.compName }}</td>
+						<td>{{ user.compNum }}</td>
+						<td>{{ user.email }}</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+	data() {
+		return {
+			users: [],
+		};
+	},
+	created() {
+		axios.get("http://localhost:5000/users/userlistcomp").then((response) => {
+			this.users = response.data;
+		});
+	},
+};
+</script>
+
+<style scoped>
+td {
+	background-color: white;
+}
+</style>
