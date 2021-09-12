1b8df6ded94f7198d217217c70ed09e9446e4cf0
ZiYooh <natsucomi42@gmail.com>
2021-09-12 15:29:58 +0900

    회원/기업 리스트 출력 디자인 수정

--- front/src/components/jayoon/UserListcomp.vue
+++ front/src/components/jayoon/UserListcomp.vue
@@ -1,29 +1,31 @@
 <template>
-	<div class="container">
-		<div class="jumbotron mt-5">
-			<div class="col-sm-8 mx-auto">
-				<h1 class="text-center">User List</h1>
-			</div>
-			<table class="table mt-5 mx-auto">
-				<thead class="thead-dark">
-					<tr>
-						<th scope="col">#</th>
-						<th scope="col">기업명</th>
-						<th scope="col">기업번호</th>
-						<th scope="col">이메일</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr v-for="(user, index) in users" :key="user.id">
-						<td>{{ index + 1 }}</td>
-						<td>{{ user.compName }}</td>
-						<td>{{ user.compNum }}</td>
-						<td>{{ user.email }}</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
+	<v-main>
+		<h1 class="text-center">User List</h1>
+		<v-row>
+			<v-col md="6" class="mt-5 mx-auto">
+				<v-simple-table>
+          <template v-slot:default>
+						<thead>
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col">기업명</th>
+								<th scope="col">기업번호</th>
+								<th scope="col">이메일</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr v-for="(user, index) in users" :key="user.id">
+								<td>{{ index + 1 }}</td>
+								<td>{{ user.compName }}</td>
+								<td>{{ user.compNum }}</td>
+								<td>{{ user.email }}</td>
+							</tr>
+						</tbody>
+					</template>
+				</v-simple-table>
+			</v-col>
+		</v-row>
+	</v-main>
 </template>
 
 <script>
